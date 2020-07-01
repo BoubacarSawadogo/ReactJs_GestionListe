de26bbc14087369f66551226ff13ef350f48b3bb
Boubacar Sawadogo <bsawadog@isimtl.local>
2020-07-01 16:52:55 -0400

    add client

--- src/components/ListeClients.js
+++ src/components/ListeClients.js
@@ -3,12 +3,46 @@ import React, { Component } from "react";
 class ListeClients extends Component {
   state = {
     title: "Liste des clients",
+    nouveauClient: "",
     clients: [
       { id: 1, nom: "Francis Bibeau" },
       { id: 2, nom: "Moctar Tolman" },
       { id: 3, nom: "Karin Benzema" },
     ],
   };
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      nouveauClient: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const clients = this.state.clients.slice();
+    console.log(clients);
+
+    if (this.state.nouveauClient !== "") {
+      clients.push({
+        id: new Date().getSeconds(),
+        nom: this.state.nouveauClient,
+      });
+    }
+    this.setState({
+      clients: clients,
+      nouveauClient: "",
+    });
+  };
+
+  handleDelete = (id) => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(function (client) {
+      return client.id === id;
+    });
+    console.log(index);
+  };
+
   render() {
     return (
       <div>
@@ -16,12 +50,18 @@ class ListeClients extends Component {
         <ul>
           {this.state.clients.map((client) => (
             <li>
-              {client.nom} <button>X</button>
+              {client.nom}{" "}
+              <button onClick={() => this.handleDelete(client.id)}>X</button>
             </li>
           ))}
         </ul>
-        <form>
-          <input type="subibmit" placeholder="Ajouter un client " />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Ajouter un client"
+            value={this.state.nouveauClient}
+            onChange={this.handleChange}
+          />
           <button>Confirmer</button>
         </form>
       </div>
